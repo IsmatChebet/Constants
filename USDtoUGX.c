bf6a855abdc74ecb6f834487a48d62a8d9d94d10
IsmatChebet <chebetismat@gmail.com>
2025-06-15 09:46:07 +0300

    Branch2: 2nd Commit

--- USDtoUGX.c
+++ USDtoUGX.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(){
+
+int main() {
+
+    const double UGX_TO_USD_RATE = 3780.0;
+
+    double usdAmount = 700.0;
+    double totalUGX;
+
+    totalUGX = usdAmount * UGX_TO_USD_RATE;
+
+    printf("\nProblem 3: USD to UGX Exchange Calculation :\n");
+    printf("Amount to exchange: %.2f USD\n", usdAmount);
+    printf("Exchange Rate: %.2f UGX/USD\n", UGX_TO_USD_RATE);
+    printf("Total UGX for %.2f USD is: %.2f UGX\n", usdAmount, totalUGX);
 
 
+    return 0;
 }
