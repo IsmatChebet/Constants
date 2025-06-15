499a0641eab3340f7de40b79a56dfee1a2d2dc94
IsmatChebet <chebetismat@gmail.com>
2025-06-15 10:53:23 +0300

    Branch4: 2nd Commit

--- EMPLOYEESBONUS.c
+++ EMPLOYEESBONUS.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
 
+int main() {
+    const double BonusPercentage = 0.15;
+    double Salary = 1500000.0;
+
+
+    float EmployeesBonus = BonusPercentage * Salary;
+
+    printf("Salary: %.2lf \n", Salary);
+    printf("BonusPercentage: %.2lf \n", BonusPercentage * 100);
+    printf("EmployeesBonus is: %.2lf \n", EmployeesBonus);
+
+
+    return 0;
 }
-
