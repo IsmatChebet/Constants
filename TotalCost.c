59e8e2217d29ac0db3f2327d946c1d7bb3ff9800
IsmatChebet <chebetismat@gmail.com>
2025-06-15 11:28:22 +0300

    Branch#5: 2nd Commit

--- TotalCost.c
+++ TotalCost.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
+
+int main() {
+
+    const double SALES_TAX_RATE = 0.18;
+
+    printf("\nProblem 5: Total Cost with Sales Tax Calculation ---\n");
+
+    double productPrice_p5 = 120000.0;
+    double salesTaxAmount;
+    double totalCost;
+
+    salesTaxAmount = SALES_TAX_RATE * productPrice_p5;
+    totalCost = productPrice_p5 + salesTaxAmount;
+
+    printf("Product Price : %.2f UGX\n", productPrice_p5);
+
+    printf("Sales Tax Rate : %.2f%%\n", SALES_TAX_RATE * 100);
+    printf("Sales Tax Amount : %.2f UGX\n", salesTaxAmount);
+    printf("Total Cost (including tax) is : %.2f UGX\n", totalCost);
+    printf("---------------------------------------------\n");
 
     return 0;
 }
