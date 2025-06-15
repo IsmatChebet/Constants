c6e3df6ed921bd6b334dacacab60eb100d92d6c3
IsmatChebet <chebetismat@gmail.com>
2025-06-15 08:12:04 +0300

    Branch2: 3rd Commit

--- DiscountPrice.c
+++ DiscountPrice.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int main() {
+
+    float originalPrice;
+    const float discountPercentage;
+    float discountedPrice;
+
+    printf("Enter the Original Price: ");
+    scanf("%f", &originalPrice);
+
+    printf("Enter the Discount Percentage: ");
+    scanf("%f", &discountPercentage);
+
+    discountedPrice = originalPrice * (1 - (discountPercentage / 100));
+
+    printf("\n--- Discount Calculation ---");
+    printf("\nOriginal Price: %.2f", originalPrice);
+    printf("\nDiscount Percentage: %.2f%%", discountPercentage);
+    printf("\nDiscounted Price: %.2f", discountedPrice);
+
+
+    return 0;
+}
